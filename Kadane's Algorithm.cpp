33cb65cb8c2727c43b538ef08a18f1db7da5fdf4
Ketan_Sharma <146920220+ketan270@users.noreply.github.com>
2024-12-05 21:37:35 +0530

    Create Kadane's Algorithm.cpp

---
+++ Kadane's Algorithm.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr) {
+        int sum=0;
+        int maxi = arr[0];
+        for(int i=0;i<arr.size();i++){
+            sum +=arr[i];
+            maxi = max(maxi,sum);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maxi;
+    }
+};
