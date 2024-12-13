986e43640afb9b502fd26b4c5f6385a0cb570789
Ketan_Sharma <146920220+ketan270@users.noreply.github.com>
2024-12-13 22:37:39 +0530

    Create Search Pattern (KMP-Algorithm).cpp

---
+++ Search Pattern (KMP-Algorithm).cpp
@@ -0,0 +1,83 @@
+class Solution {
+  public:
+    void constructLps(string &pat, vector<int> &lps) {
+
+   
+    int len = 0;
+
+    
+    lps[0] = 0;
+
+    int i = 1;
+    while (i < pat.length()) {
+
+       
+        if (pat[i] == pat[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+
+    
+        else {
+            if (len != 0) {
+
+                
+                len = lps[len - 1];
+            }
+            else {
+
+                // If no matching prefix found, set lps[i] to 0
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+vector<int> search(string &pat, string &txt) {
+    int n = txt.length();
+    int m = pat.length();
+
+    vector<int> lps(m);
+    vector<int> res;
+
+    constructLps(pat, lps);
+
+    // Pointers i and j, for traversing
+    // the text and pattern
+    int i = 0;
+    int j = 0;
+
+    while (i < n) {
+
+        // If characters match, move both pointers forward
+        if (txt[i] == pat[j]) {
+            i++;
+            j++;
+
+            // If the entire pattern is matched
+            // store the start index in result
+            if (j == m) {
+                res.push_back(i - j);
+
+                // Use LPS of previous index to
+                // skip unnecessary comparisons
+                j = lps[j - 1];
+            }
+        }
+
+        // If there is a mismatch
+        else {
+
+            // Use lps value of previous index
+            // to avoid redundant comparisons
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i++;
+        }
+    }
+    return res;
+}
+};
