d01ee2fc8853fdbec4aae6e342542f8f09cbb97c
Ketan_Sharma <146920220+ketan270@users.noreply.github.com>
2024-12-27 21:07:18 +0530

    Create K-th element of two Arrays.cpp

---
+++ K-th element of two Arrays.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    int kthElement(vector<int> &a, vector<int> &b, int k) {
+    int n = a.size(), m = b.size();
+ 
+    int last = 0;
+    int i = 0, j = 0;
+
+    for (int d = 0; d < k; ++d) {
+        if (i < n) {
+          
+            if (j < m && a[i] > b[j]) {
+                last = b[j];
+                j++;
+            } 
+            // Otherwise increment i
+            else {
+                last = a[i];
+                i++;
+            }
+        }
+
+        else if (j < m) {
+            last = b[j];
+            j++;
+        }
+    }
+
+    return last;
+}
+};
