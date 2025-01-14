e9a3deddead19f596861081b139963840e968071
Ketan_Sharma <146920220+ketan270@users.noreply.github.com>
2025-01-14 22:24:11 +0530

    Create Count Subarrays with given XOR.cpp

---
+++ Count Subarrays with given XOR.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    int subarrayXor(vector<int>& arr, int k) {
+    int res = 0; 
+
+    
+    unordered_map<int, int> mp;
+  
+      int prefXOR = 0;
+  
+    for (int val : arr) {
+      
+        prefXOR ^= val;
+
+        res = res + mp[prefXOR ^ k];
+
+
+        if (prefXOR == k)
+            res++;
+
+
+        mp[prefXOR]++;
+    }
+
+
+    return res;
+}
+
+};
