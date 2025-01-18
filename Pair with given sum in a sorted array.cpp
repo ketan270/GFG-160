edaea4ff0aff28a7eb53088d5505112405b033ed
Ketan_Sharma <146920220+ketan270@users.noreply.github.com>
2025-01-18 22:17:36 +0530

    Create Pair with given sum in a sorted array.cpp

---
+++ Pair with given sum in a sorted array.cpp
@@ -0,0 +1,45 @@
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+    int res = 0;
+    int n = arr.size();
+    int left = 0, right = n - 1;
+
+    while (left < right) {
+      
+        
+        if (arr[left] + arr[right] < target)
+            left++;
+
+
+        else if (arr[left] + arr[right] > target)
+            right--;
+
+     
+        else {
+          
+            int cnt1 = 0, cnt2 = 0;
+            int ele1 = arr[left], ele2 = arr[right];
+          
+            while (left <= right and arr[left] == ele1) {
+                left++;
+                cnt1++;
+            }
+          
+         
+            while(left <= right and arr[right] == ele2) {
+                right--;
+                cnt2++;
+            }
+           
+            if(ele1 == ele2) 
+                res += (cnt1 * (cnt1 - 1))/2;
+          
+            
+            else 
+                res += (cnt1 * cnt2);
+        }
+    }
+    return res;
+}
+};
