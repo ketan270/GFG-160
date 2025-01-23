0e08a7c3c2a7045cb7c5aec97dbb3425659677f2
Ketan_Sharma <146920220+ketan270@users.noreply.github.com>
2025-01-23 22:17:50 +0530

    Create Trapping Rain Water.cpp

---
+++ Trapping Rain Water.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    int maxWater(vector<int>& arr) {
+    stack<int> st;  
+    int res = 0;
+    for (int i = 0; i < arr.size(); i++) {
+       
+    
+        while (!st.empty() && arr[st.top()] < arr[i]) {          
+            
+            int pop_height = arr[st.top()];
+            st.pop();
+          
+            if (st.empty())
+                break;
+
+           
+            int distance = i - st.top() - 1;
+          
+
+            int water = min(arr[st.top()], arr[i]) - pop_height;
+
+            res += distance * water;
+        }
+        st.push(i);
+    }
+    return res;
+}
+};
