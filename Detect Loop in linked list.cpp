27fcbec1ecf0069955b3fb52221309659a4aeb24
Ketan_Sharma <146920220+ketan270@users.noreply.github.com>
2025-02-04 21:08:58 +0530

    Create Detect Loop in linked list.cpp

---
+++ Detect Loop in linked list.cpp
@@ -0,0 +1,33 @@
+// User function template for C++
+
+/*
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+class Solution {
+  public:
+    // Function to check if the linked list has a loop.
+    bool detectLoop(Node* head) {
+  
+    Node *slow = head, *fast = head;
+
+    while (slow && fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            return true;
+        }
+    }
+    return false;
+}
+
+};
